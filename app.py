74d5e6feb79bea02cfda9cda0d817c69f5162de5
Ariel ohayon <ariel@ao-fin.co.il>
2024-10-30 11:09:46 +0200

    ghjm

--- app.py
+++ app.py
@@ -86,4 +86,6 @@ def index():
     return render_template('index.html', agents=initial_agents)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # הגדרת השרת לעבוד על הפורט הנכון
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
